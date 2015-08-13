7af2a9d91c2c63b2b49fe6f594f10df70d5729bc
Miren Pau <miren_pau23@hotmail.co.uk>
2015-08-13 22:09:07 +0100

    Set up a simple express server

---
+++ app/routes.js
@@ -0,0 +1,35 @@
+ // app/routes.js
+
+// grab the nerd model we just created
+var Player = require('./db/tables/player');
+
+    module.exports = function(app) {
+
+        // server routes ===========================================================
+        // handle things like api calls
+        // authentication routes
+
+        // sample api route
+        app.get('/api/player', function(req, res) {
+            // use mongoose to get all nerds in the database
+            Player.find(function(err, player) {
+
+                // if there is an error retrieving, send the error. 
+                                // nothing after res.send(err) will execute
+                if (err)
+                    res.send(err);
+
+                res.json(player); // return all nerds in JSON format
+            });
+        });
+
+        // route to handle creating goes here (app.post)
+        // route to handle delete goes here (app.delete)
+
+        // frontend routes =========================================================
+        // route to handle all angular requests
+        app.get('*', function(req, res) {
+            res.sendFile(__dirname + '/public/index.html'); // load our public/index.html file
+        });
+
+    };
--- app/src/stateRouter.js
+++ app/src/stateRouter.js
@@ -1,21 +1,23 @@
-NflPredictionsApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+NflPredictionsApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
 
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-        .state('home',{
+        .state('home', {
             name: 'home',
             url: '/',
             templateUrl: 'views/home.html',
             controller: 'RootCtrl'
         })
-        .state('teams',{
+        .state('teams', {
             name: 'home.teams',
             url: '/teams',
             templateUrl: 'views/teams.html',
             controller: 'TeamsCtrl'
         })
-        .state('games',{
+        .state('games', {
             name: 'home.games',
             url: '/games',
             templateUrl: 'views/games.html',
