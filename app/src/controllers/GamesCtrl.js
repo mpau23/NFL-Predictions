ab8762325280f62594ba479d21f2f11132112fc1
Miren Pau <miren_pau23@hotmail.co.uk>
2015-08-31 19:49:16 +0100

    Cleaning up a bit

--- app/src/controllers/GamesCtrl.js
+++ app/src/controllers/GamesCtrl.js
@@ -1,180 +1,182 @@
-NflPredictionsApp.controller('GamesCtrl', ['$scope', '$rootScope', '$http', '$q', '$stateParams', 'Game', 'Team', 'Prediction', function($scope, $rootScope, $http, $q, $stateParams, Game, Team, Prediction) {
+NflPredictionsApp.controller('GamesCtrl', ['$scope', '$rootScope', '$http', '$q', '$stateParams', 'Game', 'Team', 'Prediction',
+    function($scope, $rootScope, $http, $q, $stateParams, Game, Team, Prediction) {
 
-    $scope.games = getGames();
+        $scope.games = getGames();
 
-    function getGames() {
+        function getGames() {
 
-        var gamesArray = new Array;
+            var gamesArray = new Array;
 
-        $http.get('/api/game/' + $stateParams.week)
-            .then(function(response) {
+            $http.get('/api/game/' + $stateParams.week)
+                .then(function(response) {
 
-                angular.forEach(response.data, function(value, key) {
-                    date = new Date(value.date);
-                    date.setHours(date.getHours() + 5);
-                    gamesArray.push(
-                        new Game(
-                            value._id,
-                            value.week,
-                            date.toString(),
-                            new Team(
-                                value.awayTeam._id,
-                                value.awayTeam.shortName,
-                                value.awayTeam.fullName),
-                            new Team(value.homeTeam._id,
-                                value.homeTeam.shortName,
-                                value.homeTeam.fullName)
-                        )
-                    );
-                });
-
-            })
-            .then(function() {
-                angular.forEach(gamesArray, function(value, key) {
-                    $http.get('/api/prediction/' + value.id + "/" + $rootScope.currentUser).then(function(response) {
-                        if (response.data) {
-                            value.prediction = new Prediction(response.data.homePrediction, response.data.awayPrediction, response.data.joker);
-                        } else {
-                            value.prediction = new Prediction(0, 0, false);
-
-                        }
+                    angular.forEach(response.data, function(value, key) {
+                        date = new Date(value.date);
+                        date.setHours(date.getHours() + 5);
+                        gamesArray.push(
+                            new Game(
+                                value._id,
+                                value.week,
+                                date.toString(),
+                                new Team(
+                                    value.awayTeam._id,
+                                    value.awayTeam.shortName,
+                                    value.awayTeam.fullName),
+                                new Team(value.homeTeam._id,
+                                    value.homeTeam.shortName,
+                                    value.homeTeam.fullName)
+                            )
+                        );
                     });
 
                 })
+                .then(function() {
+                    angular.forEach(gamesArray, function(value, key) {
+                        $http.get('/api/prediction/' + value.id + "/" + $rootScope.currentUser).then(function(response) {
+                            if (response.data) {
+                                value.prediction = new Prediction(response.data.homePrediction, response.data.awayPrediction, response.data.joker);
+                            } else {
+                                value.prediction = new Prediction(0, 0, false);
 
-            });
-
-        return gamesArray;
-    }
-
-    $scope.pushGames = function() {
-
-        var scheduleData;
-        var centerData;
-        var gamesArray = new Array;
-
-
-        $http.get('/json/nfl-fixtures.json')
-            .then(function(games) {
-                scheduleData = games.data.Schedule;
-                centerData = games.data.centerIds;
-
-                var gameCount = 0;
-                var thisWeeksGames;
-                var thisGameId;
-
-                angular.forEach(scheduleData, function(value, key) {
-
-                    angular.forEach(centerData, function(value1, key1) {
-                        if (value1.week === value.gameWeek)
-                            thisWeeksGames = value1;
-
-                        angular.forEach(thisWeeksGames.games, function(value2, key2) {
-                            if (value2.home === value.homeTeam)
-                                thisGameId = value2.id;
+                            }
                         });
 
-                    });
+                    })
 
-                    if (!thisWeeksGames.games[gameCount])
-                        gameCount = 0;
-
-                    gamesArray.push(new Game(
-                        thisGameId,
-                        value.gameWeek,
-                        value.gameDate + " " + value.gameTimeET,
-                        value.awayTeam,
-                        value.homeTeam));
-                    gameCount++;
                 });
 
-                angular.forEach(gamesArray, function(value, key) {
-                    $http.post('/api/game', {
-                        "id": value.id,
-                        "week": value.week,
-                        "homeTeam": value.homeTeamId,
-                        "awayTeam": value.awayTeamId,
-                        "date": value.date
-                    });
-                });
+            return gamesArray;
+        }
 
-            });
+        $scope.pushGames = function() {
+
+            var scheduleData;
+            var centerData;
+            var gamesArray = new Array;
 
 
-    };
+            $http.get('/json/nfl-fixtures.json')
+                .then(function(games) {
+                    scheduleData = games.data.Schedule;
+                    centerData = games.data.centerIds;
 
-    $scope.submit = function() {
+                    var gameCount = 0;
+                    var thisWeeksGames;
+                    var thisGameId;
 
+                    angular.forEach(scheduleData, function(value, key) {
 
-        angular.forEach($scope.games, function(value, key) {
+                        angular.forEach(centerData, function(value1, key1) {
+                            if (value1.week === value.gameWeek)
+                                thisWeeksGames = value1;
 
-            $http.post('/api/prediction', {
-                "user": $rootScope.currentUser,
-                "game": value.id,
-                "homePrediction": value.prediction.homePrediction,
-                "awayPrediction": value.prediction.awayPrediction,
-                "joker": value.prediction.joker
-            });
+                            angular.forEach(thisWeeksGames.games, function(value2, key2) {
+                                if (value2.home === value.homeTeam)
+                                    thisGameId = value2.id;
+                            });
 
+                        });
 
-        });
+                        if (!thisWeeksGames.games[gameCount])
+                            gameCount = 0;
 
-
-
-    };
-
-    $scope.jokerChange = function(game) {
-
-        angular.forEach($scope.games, function(value, key) {
-
-            if (game != value) {
-                value.prediction.joker = false;
-            }
-
-
-
-        });
-    }
-
-    function getCenterData() {
-
-        var rawData = new Array;
-
-        for (var i = 1; i <= 17; i++) {
-            $http.get('http://www.nfl.com/schedules/2015/REG' + i)
-                .then(function(response) {
-                    var weekData = response.data;
-                    var parser = new DOMParser();
-                    var doc = parser.parseFromString(weekData, 'text/html');
-                    var elements = doc.getElementsByClassName('schedules-list-content');
-
-                    var weekArray = new Array;
-
-                    angular.forEach(elements, function(value, key) {
-                        var game = {
-                            id: value.getAttribute("data-gameid"),
-                            home: value.getAttribute("data-home-abbr")
-                        };
-                        weekArray.push(game);
+                        gamesArray.push(new Game(
+                            thisGameId,
+                            value.gameWeek,
+                            value.gameDate + " " + value.gameTimeET,
+                            value.awayTeam,
+                            value.homeTeam));
+                        gameCount++;
                     });
 
-                    var regex = new RegExp("REG(.*)/");
-                    var week = regex.exec(elements[0].getAttribute("data-gc-url"));
+                    angular.forEach(gamesArray, function(value, key) {
+                        $http.post('/api/game', {
+                            "id": value.id,
+                            "week": value.week,
+                            "homeTeam": value.homeTeamId,
+                            "awayTeam": value.awayTeamId,
+                            "date": value.date
+                        });
+                    });
 
-                    var jsonCenterData = {
-
-                        week: week[1],
-                        games: weekArray
-                    };
-
-                    rawData.push(jsonCenterData);
                 });
+
 
         };
 
+        $scope.submit = function() {
 
-        return rawData;
+
+            angular.forEach($scope.games, function(value, key) {
+
+                $http.post('/api/prediction', {
+                    "user": $rootScope.currentUser,
+                    "game": value.id,
+                    "homePrediction": value.prediction.homePrediction,
+                    "awayPrediction": value.prediction.awayPrediction,
+                    "joker": value.prediction.joker
+                });
+
+
+            });
+
+
+
+        };
+
+        $scope.jokerChange = function(game) {
+
+            angular.forEach($scope.games, function(value, key) {
+
+                if (game != value) {
+                    value.prediction.joker = false;
+                }
+
+
+
+            });
+        }
+
+        function getCenterData() {
+
+            var rawData = new Array;
+
+            for (var i = 1; i <= 17; i++) {
+                $http.get('http://www.nfl.com/schedules/2015/REG' + i)
+                    .then(function(response) {
+                        var weekData = response.data;
+                        var parser = new DOMParser();
+                        var doc = parser.parseFromString(weekData, 'text/html');
+                        var elements = doc.getElementsByClassName('schedules-list-content');
+
+                        var weekArray = new Array;
+
+                        angular.forEach(elements, function(value, key) {
+                            var game = {
+                                id: value.getAttribute("data-gameid"),
+                                home: value.getAttribute("data-home-abbr")
+                            };
+                            weekArray.push(game);
+                        });
+
+                        var regex = new RegExp("REG(.*)/");
+                        var week = regex.exec(elements[0].getAttribute("data-gc-url"));
+
+                        var jsonCenterData = {
+
+                            week: week[1],
+                            games: weekArray
+                        };
+
+                        rawData.push(jsonCenterData);
+                    });
+
+            };
+
+
+            return rawData;
+        }
+
+
     }
-
-
-}]);
+]);
