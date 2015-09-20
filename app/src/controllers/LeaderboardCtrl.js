b86e2d312d4e91db44b27cddc05b0125ce7a935d
Miren Pau <miren_pau23@hotmail.co.uk>
2015-09-20 21:15:14 +0100

    Adjusting sort function

--- app/src/controllers/LeaderboardCtrl.js
+++ app/src/controllers/LeaderboardCtrl.js
@@ -3,8 +3,6 @@ NflPredictionsApp.controller('LeaderboardCtrl', ['$scope', '$http', '$q', 'Resul
 
         var userArray = new Array();
 
-        $scope.users = userArray;
-
         generateLeaderboard();
 
         function calculatePoints(game, awayScore, homeScore) {
@@ -81,90 +79,100 @@ NflPredictionsApp.controller('LeaderboardCtrl', ['$scope', '$http', '$q', 'Resul
 
             $q.all([usersPromise, gamesPromise]).then(function(response) {
 
-                    var users = response[0].data;
-                    var games = response[1].data;
+                var users = response[0].data;
+                var games = response[1].data;
 
-                    var predictionPromises = new Array();
+                var predictionPromises = new Array();
 
-                    angular.forEach(users, function(value, key) {
+                angular.forEach(users, function(value, key) {
 
-                        var user = new User(value.fullName, value.username);
+                    var user = new User(value.fullName, value.username);
 
-                        var predictionPromise = $http.get('/api/predictions/user/' + value.username)
-                            .then(function(response) {
+                    var predictionPromise = $http.get('/api/predictions/user/' + value.username)
+                        .then(function(response) {
 
-                                angular.forEach(response.data, function(prediction, key) {
-                                    user.addPrediction(prediction);
-                                });
-
+                            angular.forEach(response.data, function(prediction, key) {
+                                user.addPrediction(prediction);
                             });
-
-                        predictionPromises.push(predictionPromise);
-                        userArray.push(user);
-
-                    });
-
-                    $q.all(predictionPromises).then(function() {
-
-                        angular.forEach(games, function(game, key) {
-
-                            var gameDate = new Date(game.date);
-                            gameDate.setHours(gameDate.getHours() + 5);
-                            var gameAwayScore = 0;
-                            var gameHomeScore = 0;
-
-                            if (game.hasOwnProperty('homeScore') && game.hasOwnProperty('awayScore')) {
-
-                                gameAwayScore = game.awayScore;
-                                gameHomeScore = game.homeScore;
-
-                                calculatePoints(game, gameAwayScore, gameHomeScore);
-
-                            } else {
-
-                                $http.get('/api/results/' + game._id)
-                                    .then(function(response) {
-
-                                        tempGameResultArray = new Array();
-                                        angular.forEach(response.data, function(result, key) {
-                                            tempGameResultArray.push(result);
-                                        });
-
-                                        gameAwayScore = tempGameResultArray[0].away.score.T;
-                                        gameHomeScore = tempGameResultArray[0].home.score.T;
-
-
-                                        var finishedGameTime = new Date(gameDate);
-                                        finishedGameTime.setHours(finishedGameTime.getHours() + 6);
-
-                                        if (date > finishedGameTime) {
-                                            $http.post('/api/update/game/score', {
-                                                "game": game._id,
-                                                "homeScore": gameHomeScore,
-                                                "awayScore": gameAwayScore
-                                            });
-                                        }
-
-                                    })
-                                    .then(function() {
-                                        calculatePoints(game, gameAwayScore, gameHomeScore);
-                                    });
-
-                            }
 
                         });
 
-                    }).then(function() {
+                    predictionPromises.push(predictionPromise);
+                    userArray.push(user);
+
+                });
+
+                $q.all(predictionPromises).then(function() {
+
+                    var apiPromises = new Array();
+
+                    angular.forEach(games, function(game, key) {
+
+                        var gameDate = new Date(game.date);
+                        gameDate.setHours(gameDate.getHours() + 5);
+                        var gameAwayScore = 0;
+                        var gameHomeScore = 0;
+
+                        if (game.hasOwnProperty('homeScore') && game.hasOwnProperty('awayScore')) {
+
+                            gameAwayScore = game.awayScore;
+                            gameHomeScore = game.homeScore;
+
+                            calculatePoints(game, gameAwayScore, gameHomeScore);
+
+                        } else {
+
+                            var apiPromise = $http.get('/api/results/' + game._id)
+                                .then(function(response) {
+
+                                    tempGameResultArray = new Array();
+                                    angular.forEach(response.data, function(result, key) {
+                                        tempGameResultArray.push(result);
+                                    });
+
+                                    gameAwayScore = tempGameResultArray[0].away.score.T;
+                                    gameHomeScore = tempGameResultArray[0].home.score.T;
+
+
+                                    var finishedGameTime = new Date(gameDate);
+                                    finishedGameTime.setHours(finishedGameTime.getHours() + 6);
+
+                                    if (date > finishedGameTime) {
+                                        $http.post('/api/update/game/score', {
+                                            "game": game._id,
+                                            "homeScore": gameHomeScore,
+                                            "awayScore": gameAwayScore
+                                        });
+                                    }
+
+                                })
+                                .then(function() {
+                                    calculatePoints(game, gameAwayScore, gameHomeScore);
+                                });
+
+
+                            apiPromises.push(apiPromise);
+
+                        }
+
+                        console.log("test");
+
+                    });
+
+                    $q.all(apiPromises).then(function() {
                         userArray.sort(function(a, b) {
                             return (a.points < b.points) ? 1 : ((b.points < a.points) ? -1 : 0);
                         });
+                        console.log("finished");
+
+                        $scope.users = userArray;
+
                     });
 
-                })
-                .then(function() {
 
-                    return userArray;
                 });
+
+            });
         }
     }
 ]);
