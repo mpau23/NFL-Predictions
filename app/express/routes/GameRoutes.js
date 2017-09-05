c2b86e96e1a2bddfcae102d66a6bc39936ea85ae
Miren Pau <miren_pau23@hotmail.co.uk>
2017-09-05 11:17:17 +0100

    Updating logic for this years games

--- app/express/routes/GameRoutes.js
+++ app/express/routes/GameRoutes.js
@@ -81,46 +81,100 @@ module.exports = function(app) {
 
     app.get('/api/import/game/week/:week', function(req, res) {
 
-        var week = req.params.week;
+        request("https://raw.githubusercontent.com/BurntSushi/nflgame/master/nflgame/schedule.json", function(requestError, response, body) {
 
-        request("https://raw.githubusercontent.com/BurntSushi/nflgame/master/nflgame/schedule.json", function(error, response, body) {
-
-            if (error) {
-                res.send(error);
+            if (requestError) {
+                res.send(requestError);
             } else {
 
                 var data = JSON.parse(response.body);
-                var games = data.games;
+                var filteredGames = data.games.filter(function(element) {
+                    return element[1].year >= 2017 &&
+                        element[1].month >= 9 &&
+                        element[1].week == req.params.week;
+                });
+                winston.info("Number of filtered games: " + filteredGames.length)
 
 
                 Game.find({
-                        week: req.params.week
-                    })
-                    .exec(function(err, weekGames) {
-                        if (err) {
-                            res.send(err);
-                        } else {
+                    week: req.params.week
+                })
+                .exec(function(err, weekGames) {
+                    if (err) {
+                       res.send(err);
+                    } else {
 
-                            weekGames.forEach(function(weekGame, i) {
-                                games.forEach(function(game, j) {
-                                    if (game[0] == weekGame._id) {
-                                        var currentTimeInET = new Date(game[1].year + "-" + game[1].month + "-" + game[1].day + " " + game[1].time);
-                                        currentTimeInET.setTime(currentTimeInET.getTime() + (17 * 60 * 60 * 1000));
-                                        console.log(currentTimeInET);
-                                        weekGame.date = currentTimeInET;
-                                        weekGame.save(function(err) {
+                        if(weekGames.length > 0) {
+
+                            weekGames.forEach(function(currentGame, index) {
+                                filteredGames.forEach(function(currentFilteredGame, filteredIndex) {
+                                    
+                                    var currentTimeInET = new Date(currentFilteredGame[1].year + "-" + currentFilteredGame[1].month + "-" + currentFilteredGame[1].day + " " + currentFilteredGame[1].time);
+                                    currentTimeInET.setTime(currentTimeInET.getTime() + (17 * 60 * 60 * 1000));
+
+                                    if(currentFilteredGame[0] == currentGame._id) {
+                                        currentGame.date = currentTimeInET;
+
+                                        winston.info("Attempting to update game:" + newGame);
+
+
+                                        currentGame.save(function(err) {
                                             if (err) {
-                                                winston.info(err);
+                                                winston.info("Error updating game:" + currentFilteredGame[0]);
+                                            } else {
+                                                winston.info("Successfully updated game: " + currentFilteredGame[0]);
                                             }
                                         });
 
+                                    } else {
+                                        var newGame = new Game({
+                                            _id: currentFilteredGame[0],
+                                            week: req.params.week,
+                                            homeTeam: currentFilteredGame[1].home,
+                                            awayTeam: currentFilteredGame[1].away,
+                                            date: currentTimeInET
+                                        });
+
+                                        winston.info("Attempting to save game:" + newGame);
+
+                                        newGame.save(function(err) {
+                                            if (err) {
+                                                winston.info("Error saving game:" + currentFilteredGame[0]);
+                                            } else {
+                                                winston.info("Successfully saved game: " + currentFilteredGame[0]);
+                                            }
+                                        });
                                     }
-                                });
+                                })
                             });
 
-                            res.send(weekGames);
+                        } else {
+                            filteredGames.forEach(function(currentGame, index) {
+                                var currentTimeInET = new Date(currentGame[1].year + "-" + currentGame[1].month + "-" + currentGame[1].day + " " + currentGame[1].time);
+                                currentTimeInET.setTime(currentTimeInET.getTime() + (17 * 60 * 60 * 1000));
+
+                                var newGame = new Game({
+                                    _id: currentGame[0],
+                                    week: req.params.week,
+                                    homeTeam: currentGame[1].home,
+                                    awayTeam: currentGame[1].away,
+                                    date: currentTimeInET
+                                });
+
+                                winston.info("Attempting to save game:" + newGame);
+
+                                newGame.save(function(err) {
+                                    if (err) {
+                                        winston.info("Error saving game:" + currentGame[0]);
+                                    } else {
+                                        winston.info("Successfully saved game: " + currentGame[0]);
+                                    }
+                                });
+                            })
                         }
-                    });
+                        res.send(filteredGames);
+                    }
+                });
             }
 
         });
